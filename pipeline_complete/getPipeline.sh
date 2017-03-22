c96ac2c67f0ab08b1700f9ddbd53e64aee637e7d
niah9119 <niclas.ahlstrand@gmail.com>
2017-03-22 09:56:13 +0100

    Fixed shell scripts

--- pipeline_complete/getPipeline.sh
+++ pipeline_complete/getPipeline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+  echo "Usage: $0 <pipeline-name>"
+  exit 1
+fi
+
 pipelineName=$1
 
 java -jar jenkins-cli.jar -s http://localhost:8080/ get-job ${pipelineName} > ${pipelineName}.xml
