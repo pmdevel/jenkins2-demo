80ee418c3178947de18b5f10647ac598ada5f115
Niclas Ahlstrand <niclas.ahlstrand@pensionsmyndigheten.se>
2017-06-30 13:44:20 +0200

    Test deklarativ pipeline

--- check-out.sh
+++ check-out.sh
@@ -2,4 +2,4 @@
 export https_proxy=http://proxyw.ppm.nu:8080
 
 echo "Checking out project to directory 'build'"
-git clone https://github.com/pmdevel/jenkins2-demo-pipelines.git build
+git clone https://github.com/pmdevel/jenkins2-demo-pipelines.git .
