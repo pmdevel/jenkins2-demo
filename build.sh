43ebc2872e8737e5bec0d90238f67805ee0fe56b
Niclas Ahlstrand <niclas.ahlstrand@pensionsmyndigheten.se>
2017-06-30 15:03:58 +0200

    Test deklarativ pipeline

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 echo "Starting build on: $(hostname)"
 echo "Build number: ${BUILD_NUMBER}"
-echo ${BUILD_NUBMER} > build.res
+echo ${BUILD_NUMBER} > build.res
 sleep 3
 echo "Build ready: $(hostname)"
